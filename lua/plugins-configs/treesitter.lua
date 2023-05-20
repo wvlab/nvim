8bc764c0ee5ec2f58400e1c9f16abfc3395fd866
WVlab <tabs3536@gmail.com>
2023-05-20 22:21:55 +0300

    use lualatex in vimtex config

--- lua/plugins-configs/treesitter.lua
+++ lua/plugins-configs/treesitter.lua
@@ -1,24 +1,25 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-      "lua",
-      "c",
-      "cpp",
-      "javascript",
-      "typescript",
-      "elixir",
-      "erlang",
-      "zig",
-      "rust",
-  },
+require("nvim-treesitter.configs").setup {
+    ensure_installed = {
+        "lua",
+        "c",
+        "python",
+        "cpp",
+        "javascript",
+        "typescript",
+        "elixir",
+        "erlang",
+        "zig",
+        "rust",
+    },
 
-  sync_install = true,
+    sync_install = true,
 
-  auto_install = true,
+    auto_install = true,
 
-  ignore_install = { },
+    ignore_install = {},
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
 }
--- lua/plugins-configs/vimtex.lua
+++ lua/plugins-configs/vimtex.lua
@@ -1,1 +1,25 @@
 vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_latexmk_engines = {
+    _ = "-lualatex",
+}
+vim.g. vimtex_quickfix_ignore_filters = {
+    "Overfull",
+    "Underfull",
+}
+vim.g.vimtex = {
+    "Overfull",
+    "Underfull",
+}
+
+vim.g.vimtex_compiler_latexmk = {
+    callback = 1,
+    continious = 1,
+    executable = "latexmk",
+    options = {
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+        "-pdflua",
+    }
+}
