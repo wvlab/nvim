6c297c4e333a8187f7fb04d571f4900998e8466e
WVlab <tabs3536@gmail.com>
2023-05-20 22:12:36 +0300

    lualine config

--- lua/plugins-configs/lualine.lua
+++ lua/plugins-configs/lualine.lua
@@ -1,5 +1,18 @@
-require('lualine').setup {
+require("lualine").setup {
     options = {
-        theme = 'auto'
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = {
+            left = "|",
+            right = "|"
+        },
+    },
+    sections = {
+        lualine_a = {"mode"},
+        lualine_b = {"branch", "diff", "diagnostics"},
+        lualine_c = {"filename"},
+        lualine_x = {"encoding", "fileformat", "filetype"},
+        lualine_y = {"progress"},
+        lualine_z = {"location"}
     }
 }
